f58fe3e51f5e9e1f9042cbc2e802b1c9f4094a49
rupesss-09 <rupesss09@gmail.com>
2021-05-03 12:32:59 -0500

    Jpa Repository added. Class name StudentRepository.

--- src/main/java/com/spring/boot/student/StudentService.java
+++ src/main/java/com/spring/boot/student/StudentService.java
@@ -1,23 +1,30 @@
 package com.spring.boot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getstudents(){
-        return Arrays.asList(
+        return studentRepository.findAll();
+
+        /*return Arrays.asList(
                 new Student(
                         1L,
                         "Rupesh",
                         "rupesh@gmail.com",
                         LocalDate.of(2001, Month.MAY,24),
                         23)
-        );
+        );*/
     }
 }
